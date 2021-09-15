dc14a9f4ec2d9cccadf395c2c5e526ce31b51f12
lucas pimentel <lucaspimenteldb@gmail.com>
2021-09-15 14:49:07 -0300

    template full funcional

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
-require('./bootstrap');
+// require('./bootstrap');
 
 window.Vue = require('vue');
 
 import Vuetify from '../plugins/vuetify';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app-component', require('./components/App.vue').default);
 
 const app = new Vue({
     vuetify: Vuetify,
