cbbaca4a86115b3addc869cf53a9b9a0244e3c6b
lucas pimentel <lucaspimenteldb@gmail.com>
2021-09-15 14:38:40 -0300

    vuetify-loader e sass-loader com ajustse vas versores

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,17 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+const CaseSensitivePathsWebpackPlugin = require('case-sensitive-paths-webpack-plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+var webConfig = {
+    plugins: [
+        new VuetifyLoaderPlugin(),
+        new CaseSensitivePathsWebpackPlugin
+    ],
+}
+
+
+mix.webpackConfig(webConfig);
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
